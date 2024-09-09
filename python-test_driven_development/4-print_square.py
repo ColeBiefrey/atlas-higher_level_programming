40c53fcc0ffdb3cf590366e3ca15598502556ab9
ColeBiefrey <Cole.Kelsey@atlasschool.com>
2024-09-09 17:47:51 -0500

    trailing space

--- python-test_driven_development/4-print_square.py
+++ python-test_driven_development/4-print_square.py
@@ -5,7 +5,7 @@ This module is composed by a function that prints a square with the character #
 
 
 def print_square(size):
-    """ 
+    """
     Function that prints a square with the character #
 
     Args:
