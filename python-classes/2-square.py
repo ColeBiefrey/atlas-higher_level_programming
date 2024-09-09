7b61e7a0691c2b2283aaff53ddd6a0218ef5e628
ColeBiefrey <Cole.Kelsey@atlasschool.com>
2024-09-09 15:59:40 -0500

    fixed documentation

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 class Square:
-    """ A class that defines a square by its size
+    """ 
+    A class that defines a square by its size
     """
     def __init__(self, size=0):
-        """ Method to initialize the square object
+        """ 
+        Method to initialize the square object
         """
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
@@ -11,3 +13,4 @@ class Square:
             raise ValueError("size must be >= 0")
         else:
             self.__size = int(size)
+
