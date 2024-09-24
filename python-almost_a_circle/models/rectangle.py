78f487196d022d2876a6a33ad0f81f00fac750c2
ColeBiefrey <Cole.Kelsey@atlasschool.com>
2024-09-24 12:01:28 -0500

    dictionary properties

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -132,7 +132,7 @@ class Rectangle(Base):
             for key, value in kwargs.items():
                 setattr(self, key, value)
 
-        def to_dictionary(self):
+    def to_dictionary(self):
         """
         method that returs a dictionary with properties
         """
