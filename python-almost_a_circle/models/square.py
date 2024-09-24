65bbed29cbcee71c0f20494390503ba1d45b80b5
ColeBiefrey <Cole.Kelsey@atlasschool.com>
2024-09-24 11:05:40 -0500

    initialized square

---
+++ python-almost_a_circle/models/square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+Module that contains class Square,
+inheritance of class Rectangle
+"""
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """
+    Class Rectangle
+    """
+
+    def __init__(self, size, x=0, y=0, id=None):
+        """
+        Initializes instances
+        """
+        super().__init__(size, size, x, y, id)
